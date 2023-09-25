65a7b54d67f0a091dd915e96c41441e81bec4ebf
ethstakersclub <ethstakersclub@proton.me>
2023-09-25 04:24:40 +0200

    Minor mobile layout fixes

--- ethstakersclub/settings.py
+++ ethstakersclub/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-VERSION = "1_0_0"
+VERSION = "1_0_1"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
