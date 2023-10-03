c5ecd307357ebfd3771418acf62b6147d2eaba2b
ethstakersclub <ethstakersclub@proton.me>
2023-10-03 11:23:46 +0200

    Add search to profile admin page

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Profile
 
-# Register your models here.
-admin.site.register(Profile)
+class UserProfileAdmin(admin.ModelAdmin):
+    search_fields = ['user__email']
+
+admin.site.register(Profile, UserProfileAdmin)
